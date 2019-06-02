a62813b449968f52ffbe237acee5e47024f9d28a
Blake Watson <blake@blakewatson.com>
2019-06-02 02:03:44 -0500

    remove tmp files from being tracked

--- tmp/t9e5bm6xc7fa.duecy435g8pg.php
+++
@@ -1,8 +0,0 @@
-<?php echo $this->render('templates/header.html',NULL,get_defined_vars(),0); ?>
-
-    <div id="sheet"></div>
-
-    <input type="hidden" name="sheet_id" id="sheet-id" value="<?= ($sheet_id) ?>">
-    <input type="hidden" name="csrf" id="csrf" value="<?= ($CSRF) ?>">
-
-<?php echo $this->render('templates/footer.html',NULL,get_defined_vars(),0); ?>
