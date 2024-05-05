255d0ac841672affbed6717c0e9408b267b5040e
Blake Watson <blake@blakewatson.com>
2024-05-05 18:42:25 -0500

    prod assets

--- js/components/TextSection.vue
+++ js/components/TextSection.vue
@@ -22,6 +22,7 @@ export default {
             'personalityText',
             'backstoryText',
             'treasureText',
+            'notesText',
             'organizationsText'
         ]),
 
