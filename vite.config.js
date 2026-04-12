366b9f629dc3f55c89592c8316d240575873c793
Blake Watson <blake@blakewatson.com>
2026-04-11 19:49:50 -0500

    tweaked the vite config for vue devtools support

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,39 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 const isWatch = process.argv.includes('--watch');
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-  },
-  build: {
-    outDir: 'dist',
-    manifest: true,
-    emptyOutDir: !isWatch,
-    watch: isWatch ? { exclude: ['dist/**'] } : null,
-    rollupOptions: {
-      input: {
-        app: 'js/app.js',
-        dashboard: 'js/dashboard.js',
-        print: 'js/print.js',
-        styles: 'css/app.css',
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+
+  return {
+    plugins: [
+      vue({
+        features: {
+          prodDevtools: isDev,
+        },
+      }),
+      tailwindcss(),
+    ],
+    resolve: {
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    },
+    build: {
+      outDir: 'dist',
+      manifest: true,
+      minify: !isDev,
+      emptyOutDir: !isWatch,
+      watch: isWatch ? { exclude: ['dist/**'] } : null,
+      rollupOptions: {
+        input: {
+          app: 'js/app.js',
+          dashboard: 'js/dashboard.js',
+          print: 'js/print.js',
+          styles: 'css/app.css',
+        },
       },
     },
-  },
-  publicDir: 'public',
+    publicDir: 'public',
+  };
 });
