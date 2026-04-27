2155a1a8ba9956cfddbe6b96f1cf3645a70fa3e2
Blake Watson <blake@blakewatson.com>
2026-04-26 23:02:53 -0500

    user table migration

---
+++ migrations/005.php
@@ -0,0 +1,30 @@
+<?php
+
+define('ROOT_DIR', dirname(__DIR__));
+
+require ROOT_DIR . '/vendor/autoload.php';
+
+$db = new DB\SQL('sqlite:'.ROOT_DIR.'/data/db.sqlite3');
+
+$db->exec('PRAGMA foreign_keys = ON;');
+
+// Get existing columns to check what needs to be added
+$columns = $db->exec('PRAGMA table_info(user);');
+$columnNames = array_column($columns, 'name');
+
+// Add created_at column if it doesn't exist
+if (!in_array('created_at', $columnNames)) {
+    $db->exec('ALTER TABLE "user" ADD COLUMN "created_at" TEXT DEFAULT NULL;');
+}
+
+// Add updated_at column if it doesn't exist
+if (!in_array('updated_at', $columnNames)) {
+    $db->exec('ALTER TABLE "user" ADD COLUMN "updated_at" TEXT DEFAULT NULL;');
+}
+
+// Add deleted_at column if it doesn't exist (NULL by default for soft deletes)
+if (!in_array('deleted_at', $columnNames)) {
+    $db->exec('ALTER TABLE "user" ADD COLUMN "deleted_at" TEXT DEFAULT NULL;');
+}
+
+die;
